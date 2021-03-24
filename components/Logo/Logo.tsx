b62e915e53514cce18c6e2d2382bfe6a03cc5b6d
homfarnam <homfarnam@gmail.com>
2021-03-24 23:27:53 +0430

    feat: Change logo

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,27 +1,19 @@
 import Image from "next/image"
 import * as React from "react"
-import styled from "styled-components"
 
 interface LogoProps {
   theme?: string
 }
 
-const TriangleLogo = styled.div`
-  border-left: 15px solid transparent;
-  border-right: 15px solid transparent;
-  border-bottom: 15px solid #aaa;
-`
-
 const Logo: React.FC<LogoProps> = ({ theme = "black" }) => {
   return (
     <>
-      <TriangleLogo className="mr-1" />
       <div className="relative z-20 w-[142px]">
         {theme == "black" ? (
-          <Image src="/PHOTOSNAP.svg" layout="responsive" width="142" height="16" />
+          <Image src="/full-logo.svg" layout="responsive" width="142" height="16" />
         ) : (
           theme == "white" && (
-            <Image src="/PHOTOSNAP-white.svg" layout="responsive" width="142" height="16" />
+            <Image src="/full-white-logo.svg" layout="responsive" width="142" height="16" />
           )
         )}
       </div>
