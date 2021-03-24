64774081394fdad80aac65a9e5f7548264d2296c
homfarnam <homfarnam@gmail.com>
2021-03-24 14:25:21 +0430

    feat: Create styled components file

---
+++ styles/styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Nav = styled.nav`
+  a {
+    margin-left: 10px;
+    margin-right: 10px;
+
+    :hover {
+      opacity: 0.3;
+    }
+  }
+`
+
+export { Nav }
